a229e9df6eeb61ab543ea581529255d03f1118ea
Adam Slack <adam.slack@dazn.com>
2020-01-14 14:48:23 +0000

    lines or circles

--- process-video.js
+++ process-video.js
@@ -1,5 +1,11 @@
 const frameCanvas = document.getElementById('c1')
 const coloursCanvas = document.getElementById('c2')
+let drawStyle = 'lines'
+
+function getDrawStyle () {
+    const radios = document.getElementsByName('drawStyle')
+    drawStyle = Array.from(radios).find((radio) => radio.checked).value
+}
 
 function getAverageColour (data) {
     const length = data.length;
@@ -23,6 +29,27 @@ function getAverageColour (data) {
     return rgb
 }
 
+function drawLine(ctx, lineWidth, xStart, yStart, xEnd, yEnd, colour) {
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth
+
+    ctx.moveTo(xStart, yStart)
+    ctx.lineTo(xEnd, yEnd);
+
+    ctx.strokeStyle = `rgb(${colour.r},${colour.g},${colour.b})`
+    ctx.stroke();
+}
+
+function drawCircle(ctx, lineWidth, radius, colour) {
+    ctx.beginPath();
+    
+    ctx.lineWidth = lineWidth
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    
+    ctx.strokeStyle = `rgb(${colour.r},${colour.g},${colour.b})`
+    ctx.stroke();
+}
+
 function computeColour(frameNum) {     
     const frameCtx = frameCanvas.getContext('2d')
     frameCtx.drawImage(video, 0, 0, 220, 150)
@@ -35,14 +62,10 @@ function computeColour(frameNum) {
     const data = canvasFrame.data;
     const rgb = getAverageColour(data)
 
-    coloursCtx.beginPath();
-    coloursCtx.lineWidth = Math.ceil(coloursCanvas.height / video.duration)
-
-    coloursCtx.moveTo(0, frameNum*spacing);
-    coloursCtx.lineTo(coloursCanvas.width, frameNum*spacing);
-
-    coloursCtx.strokeStyle = `rgb(${rgb.r},${rgb.g},${rgb.b})`
-    coloursCtx.stroke();
+    const lineWidth = Math.ceil(coloursCanvas.height / video.duration) + 2
+    
+    if (drawStyle === 'lines') drawLine(coloursCtx, lineWidth, 0, frameNum*spacing, coloursCanvas.width, frameNum*spacing, rgb)
+    if (drawStyle === 'circle') drawCircle(coloursCtx, lineWidth, frameNum*spacing, rgb)
 }
 
 function playSelectedFile(event) {
@@ -57,6 +80,13 @@ const player = document.getElementById('video')
 
 let frameNum = 0
 video.addEventListener('loadeddata', function() {
+    getDrawStyle()
+    coloursCanvas.style.display = 'block';
+    const coloursCtx = coloursCanvas.getContext('2d')
+    coloursCtx.fillStyle = "black";
+    coloursCtx.fillRect(0, 0, coloursCanvas.width, coloursCanvas.height);
+    
+    frameNum = 0
     this.currentTime = frameNum
 })
 
