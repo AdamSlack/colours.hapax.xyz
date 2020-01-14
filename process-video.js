6f0cad7c6e59355af91e9e3a31c1bf9e7203c001
Adam Slack <adam.slack@dazn.com>
2020-01-14 10:11:04 +0000

    drawing lines on a canvas

---
+++ process-video.js
@@ -0,0 +1,68 @@
+const frameCanvas = document.getElementById('c1')
+const coloursCanvas = document.getElementById('c2')
+
+function computeColour(frameNum) {     
+    const frameCtx = frameCanvas.getContext('2d')
+    frameCtx.drawImage(video, 0, 0, 220, 150)
+
+    const spacing = coloursCanvas.height/video.duration
+    const coloursCtx = coloursCanvas.getContext('2d')
+
+    const canvasFrame = frameCtx.getImageData(0,0,frameCanvas.width, frameCanvas.height)
+    
+    const data = canvasFrame.data;
+    const length = data.length;
+
+    const rgb = {r:0, g:0, b:0}
+    
+    let pixelCount = 0
+    let pixelIndex = 0
+
+    while (pixelIndex < length) {
+        
+        rgb.r += data[pixelIndex];
+        rgb.g += data[pixelIndex+1];
+        rgb.b += data[pixelIndex+2];
+        
+        pixelCount++
+        pixelIndex+=4;
+    }
+    
+    // floor the average values to give correct rgb values (ie: round number values)
+    rgb.r = Math.floor(rgb.r/pixelCount);
+    rgb.g = Math.floor(rgb.g/pixelCount);
+    rgb.b = Math.floor(rgb.b/pixelCount);
+
+    coloursCtx.beginPath();
+    coloursCtx.lineWidth = 2;
+    
+    coloursCtx.moveTo(0, frameNum*spacing);
+    coloursCtx.lineTo(coloursCanvas.width, frameNum*spacing);
+
+    coloursCtx.strokeStyle = `rgb(${rgb.r},${rgb.g},${rgb.b})`
+    coloursCtx.stroke();
+}
+
+function playSelectedFile(event) {
+    var file = this.files[0]
+    player.src = URL.createObjectURL(file)
+}
+
+document.getElementById('video-selector').onchange = playSelectedFile
+
+
+const player = document.getElementById('video')
+
+let frameNum = 0
+video.addEventListener('loadeddata', function() {
+    this.currentTime = frameNum
+})
+
+video.addEventListener('seeked', function() {
+    computeColour(frameNum);
+    frameNum += 1;
+
+    if (frameNum <= this.duration) {
+        this.currentTime = frameNum;
+    }
+});
