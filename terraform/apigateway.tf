98332f7dcd0c1f7e1347e37780a093cff9a5e411
Adam Slack <admslack@github.com>
2020-08-14 08:13:45 +0100

    apigw terraform should redploy if there is a change to the endpoints

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -25,26 +25,20 @@ resource "aws_api_gateway_base_path_mapping" "www_prod" {
     domain_name = aws_api_gateway_domain_name.www_colours_hapax.domain_name
 }
 
+
 resource "aws_api_gateway_resource" "charts" {
     path_part   = "charts"
     parent_id   = aws_api_gateway_rest_api.colours_hapax.root_resource_id
     rest_api_id = aws_api_gateway_rest_api.colours_hapax.id
 }
 
+
+
 resource "aws_api_gateway_method" "get_charts" {
     rest_api_id   = aws_api_gateway_rest_api.colours_hapax.id
     resource_id   = aws_api_gateway_resource.charts.id
     http_method   = "GET"
     authorization = "NONE"
-}
-
-module "cors" {
-    source = "squidfunk/api-gateway-enable-cors/aws"
-    version = "0.3.1"
-
-    api_id          = aws_api_gateway_rest_api.colours_hapax.id
-    api_resource_id = aws_api_gateway_resource.charts.id
-    allow_methods   = ["OPTIONS", "GET", "POST"]
 }
 
 resource "aws_api_gateway_integration" "get_charts" {
@@ -55,6 +49,8 @@ resource "aws_api_gateway_integration" "get_charts" {
     type                    = "AWS_PROXY"
     uri                     = aws_lambda_function.getColourCharts.invoke_arn
 }
+
+
 
 resource "aws_api_gateway_method" "post_chart" {
     rest_api_id   = aws_api_gateway_rest_api.colours_hapax.id
@@ -72,15 +68,34 @@ resource "aws_api_gateway_integration" "post_chart" {
     uri                     = aws_lambda_function.postColourChart.invoke_arn
 }
 
+
+
+module "cors" {
+    source = "squidfunk/api-gateway-enable-cors/aws"
+    version = "0.3.1"
+
+    api_id          = aws_api_gateway_rest_api.colours_hapax.id
+    api_resource_id = aws_api_gateway_resource.charts.id
+    allow_methods   = ["OPTIONS", "GET", "POST"]
+}
+
 resource "aws_api_gateway_deployment" "colours_hapax" {
     depends_on = [
         aws_api_gateway_integration.get_charts,
         aws_api_gateway_integration.post_chart,
-
     ]
 
     rest_api_id = aws_api_gateway_rest_api.colours_hapax.id
     stage_name  = "prod"
+
+    triggers = {
+        redeployment = sha1(join(",", list(
+            jsonencode(aws_api_gateway_integration.get_charts),
+            jsonencode(aws_api_gateway_integration.post_chart),
+            jsonencode(aws_api_gateway_integration.post_chart),
+        )))
+    
+
 
     lifecycle {
         create_before_destroy = true
